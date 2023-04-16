c807fc07cddb36f1240af2c4f0f6d3c735073e7b
mybaditssam <shernandez@econnect.eco>
2023-04-16 19:10:10 -0400

    assigned character variables and added some pseudo code at the bottom to get an idea of what i want the site to do

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerchar = "abcdefghijklmnopqrstuvwxyz";
+var upperchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numberchar = "0123456789"
+var specchar = "!$%&'()*+,-./:;<=>?@[^_`{|}~"
 
 function generatePassword () {
-  prompt("hello");
+  prompt("How many digits?");
+  return "password"
 }
 
 // Write password to the #password input
@@ -16,3 +21,10 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// When generate button is clicked
+// User is prompted password criteria 
+//    - criteria must be 8 - 128 characters
+//    - at least one character type should be chosen
+// Password displays on page
